974e34742f2b0976204d4ee69c1f89aecbc9ee9e
Daniel Scott <coder@drdscott.com>
2016-08-12 16:14:20 +0100

    HTTP Verb change
    The first endpoint should not be a GET request, but a PUT one.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,7 +2,7 @@
 
 
 Route::group(['prefix'=>'api', 'middleware' => ['api'], 'namespace' => '\API'], function() {
-    Route::get('convert/{integer}', 'ApiController@convert');
+    Route::put('convert/{integer}', 'ApiController@convert');
     Route::get('recent', 'ApiController@recent');
     Route::get('top10', 'ApiController@top10');
 });
